a338f95b0fba818ba9b585845e1907fd03676362
HAFISAALBAHRI <hafisa.albahri@gmail.com>
2026-06-23 09:47:31 +0400

    add pilot Property

--- Flight Management System/Models/Pilot.cs
+++ Flight Management System/Models/Pilot.cs
@@ -4,7 +4,13 @@ using System.Text;
 
 namespace Flight_Management_System.Models
 {
-    internal class Pilot
-    {
+     public class Pilot
+        {
+            public int pilotId { get; set; }
+            public string pilotName { get; set; }
+            public string pilotPhone { get; set; }
+            public string licenseNumber { get; set; }
+            public int flightHours { get; set; }
+            public bool isAvailable { get; set; }
+        }
     }
-}
