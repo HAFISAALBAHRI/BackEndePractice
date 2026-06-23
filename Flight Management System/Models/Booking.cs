989bac87ccd7234d199743bffc8667bd61a61aa4
HAFISAALBAHRI <hafisa.albahri@gmail.com>
2026-06-23 09:50:41 +0400

    add booking property

--- Flight Management System/Models/Booking.cs
+++ Flight Management System/Models/Booking.cs
@@ -4,7 +4,16 @@ using System.Text;
 
 namespace Flight_Management_System.Models
 {
-    internal class Booking
-    {
+
+   
+        public class Booking
+        {
+            public int bookingId { get; set; }
+            public int passengerId { get; set; }
+            public int flightId { get; set; }
+            public string seatNumber { get; set; }
+            public string bookingDate { get; set; }
+            public decimal totalPrice { get; set; }
+            public string status { get; set; }
+        }
     }
-}
