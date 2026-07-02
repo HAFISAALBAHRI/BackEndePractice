c34a7f637655e07077d346d07f154ab1da6bcaec
HAFISAALBAHRI <hafisa.albahri@gmail.com>
2026-07-02 10:40:25 +0400

    add class propartiy

--- E-CommerceWebsiteSystem1/Models/Category.cs
+++ E-CommerceWebsiteSystem1/Models/Category.cs
@@ -8,5 +8,18 @@ namespace E_CommerceWebsiteSystem1.Models
 {
     internal class Category
     {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int categoryId { get; set; } // system generated
+
+        [Required]
+        [MaxLength(100)]
+        public string categoryName { get; set; } = string.Empty; // user input, unique
+
+        [MaxLength(500)]
+        public string? description { get; set; } // user input, optional
+
+        [MaxLength(300)]
+        public string? imageUrl { get; set; } // user input, optional
     }
 }
