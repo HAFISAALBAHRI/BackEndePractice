77dc3768c518cd065dedbc3e2817f6394e74f551
HAFISAALBAHRI <hafisa.albahri@gmail.com>
2026-07-02 10:38:41 +0400

    add class relationship

--- E-CommerceWebsiteSystem1/Models/Review.cs
+++ E-CommerceWebsiteSystem1/Models/Review.cs
@@ -29,5 +29,8 @@ namespace E_CommerceWebsiteSystem1.Models
 
         [Required]
         public DateTime reviewDate { get; set; } // system generated
+        public User? User { get; set; } // relationship ==> many reviews are written by one user
+
+        public Product? Product { get; set; } // relationship ==> many reviews belong to one product
     }
 }
