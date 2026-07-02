6d41576e25702b5ac8c1f2e7f9be01e1caea09b8
HAFISAALBAHRI <hafisa.albahri@gmail.com>
2026-07-02 10:32:48 +0400

    add ComponentModel for class product

--- E-CommerceWebsiteSystem1/Models/Product.cs
+++ E-CommerceWebsiteSystem1/Models/Product.cs
@@ -1,10 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
-
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 namespace E_CommerceWebsiteSystem1.Models
 {
     internal class Product
     {
+
     }
 }
