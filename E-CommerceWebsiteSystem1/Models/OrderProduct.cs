c0e9d3acaaa3ac529f1ed9d0cfd618f31fba72eb
HAFISAALBAHRI <hafisa.albahri@gmail.com>
2026-07-02 10:42:17 +0400

    add class proparity

--- E-CommerceWebsiteSystem1/Models/OrderProduct.cs
+++ E-CommerceWebsiteSystem1/Models/OrderProduct.cs
@@ -8,5 +8,20 @@ namespace E_CommerceWebsiteSystem1.Models
 {
     internal class OrderProduct
     {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int orderProductId { get; set; } // system generated
+
+        [Required]
+        [ForeignKey("Order")]
+        public int orderId { get; set; } // foreign key
+
+        [Required]
+        [ForeignKey("Product")]
+        public int productId { get; set; } // foreign key
+
+        [Required]
+        [Range(1, 999)]
+        public int quantity { get; set; } // user input
     }
 }
