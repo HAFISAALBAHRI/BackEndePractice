c57eb8e70dc5654f30e52c8ce5a06f0a1b09e2cb
HAFISAALBAHRI <hafisa.albahri@gmail.com>
2026-07-02 10:17:25 +0400

    add class Category

---
+++ E-CommerceWebsiteSystem/Models/Category.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace E_CommerceWebsiteSystem.Models
+{
+    internal class Category
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int categoryId { get; set; } // system generated
+
+        [Required]
+        [MaxLength(100)]
+        public string categoryName { get; set; } = string.Empty; // user input, unique
+
+        [MaxLength(500)]
+        public string? description { get; set; } // user input, optional
+
+        [MaxLength(300)]
+        public string? imageUrl { get; set; } // user input, optional
+
+
+        public ICollection<Product> Products { get; set; } = new List<Product>(); // relationship ==> one category has many products
+    }
+}
