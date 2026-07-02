23ec3496c0e0032618a71d021590c01c07a62b27
HAFISAALBAHRI <hafisa.albahri@gmail.com>
2026-07-02 10:15:52 +0400

    add class OrderProduct

---
+++ E-CommerceWebsiteSystem/Models/OrderProduct.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace E_CommerceWebsiteSystem.Models
+{
+    internal class OrderProduct
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int orderProductId { get; set; } // system generated
+
+        [Required]
+        [ForeignKey("Order")]
+        public int orderId { get; set; } // foreign key
+
+        [Required]
+        [ForeignKey("Product")]
+        public int productId { get; set; } // foreign key
+
+        [Required]
+        [Range(1, 999)]
+        public int quantity { get; set; } // user input
+
+        //relations
+        public Order? Order { get; set; } // relationship ==> many orderproduct belong to one order
+
+        public Product? Product { get; set; } // relationship ==> many orderproduct belong to one product
+    }
+}
---
+++ E-CommerceWebsiteSystem/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace E_CommerceWebsiteSystem
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
