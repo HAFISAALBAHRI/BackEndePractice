43c1669fde25d31c3a3ba7d9e66e891964061cb3
HAFISAALBAHRI <hafisa.albahri@gmail.com>
2026-07-02 10:35:38 +0400

    add ComponentModel

--- E-CommerceWebsiteSystem1/Models/Order.cs
+++ E-CommerceWebsiteSystem1/Models/Order.cs
@@ -1,7 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
-
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 namespace E_CommerceWebsiteSystem1.Models
 {
     internal class Order
