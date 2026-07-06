993d51d80e1044fae9e6902ec0808c185600d101
HAFISAALBAHRI <hafisa.albahri@gmail.com>
2026-07-06 15:02:53 +0400

    add E-Commerce System EF Core project

---
+++ E-CommerceSystem_EF_Core/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace E_CommerceSystem_EF_Core
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
