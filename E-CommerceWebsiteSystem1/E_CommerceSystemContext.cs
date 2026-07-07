fd88c33b48f31b3da596cac9baf15a77ae704626
HAFISAALBAHRI <hafisa.albahri@gmail.com>
2026-07-07 09:25:50 +0400

    add context

---
+++ E-CommerceWebsiteSystem1/E_CommerceSystemContext.cs
@@ -0,0 +1,23 @@
+﻿using E_CommerceWebsiteSystem1.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace E_CommerceWebsiteSystem1
+{
+    internal class ECommerceContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Order> Orders { get; set; }
+
+
+        public DbSet<OrderProduct> OrderProducts { get; set; }
+
+        public DbSet<Review> Reviews { get; set; }
+
+
+    }
+}
