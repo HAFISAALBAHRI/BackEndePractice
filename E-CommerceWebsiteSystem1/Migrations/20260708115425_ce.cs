c5acbbc122432effcaa0cb1c9701f9cd52eb8e60
HAFISAALBAHRI <hafisa.albahri@gmail.com>
2026-07-08 16:00:10 +0400

    .

---
+++ E-CommerceWebsiteSystem1/Migrations/20260708115425_ce.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace E_CommerceWebsiteSystem1.Migrations
+{
+    /// <inheritdoc />
+    public partial class ce : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
